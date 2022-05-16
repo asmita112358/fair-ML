5839c684ca916fa8adde36f9ac60a1a5892671e4
Asmita Roy <asmitaroy@client-10-229-52-128.tamulink.tamu.edu>
2022-05-16 10:36:19 -0500

    Data Generation

--- projection.R
+++ projection.R
@@ -3,21 +3,24 @@
 ##Using the data generation idea from Romano et al, Achieving Equalized odds
 
 n = 1000
-
+library(dplyr)
 
 A = sample(c(0,1), size = n, replace = T, prob = c(0.1,0.9))
-n1 = sum(A == 1)
-n0 = sum(A == 0)
-e1 = rnorm(n1)
-e0 = rnorm(n0)
-Z1 = rnorm(n1)
-Z2 = rnorm(n0)
-Y0 = 9*Z2 + e0
-Y1 = 9*Z1 + e1
+Z1 = rnorm(n)
+Z2 = rnorm(n)
+X = matrix(nrow = n, ncol = 2)
+Y = vector()
+for(i in 1:n)
+{
+  X[i,] = if_else(rep(A[i] ==0, 2), c(Z1[i], 3*Z2[i]), c(3*Z1[i], Z2[i]))
+  Y[i] = ifelse(A[i] == 0, 9*Z2[i] + rnorm(1), 9*Z1[i] + rnorm(1))
+  
+}
 
 
-A_new = c(A[A==0], A[A==1])
-data = data.frame(Y, A_new,)
 
-
-b
+obj = lm(Y ~ X)
+Y_hat = fitted.values(obj)
+RMSE_all = sqrt(mean((Y_hat - Y)^2))
+RMSE_1 = sqrt(sum(((Y_hat - Y)^2) *A)/sum(A))
+RMSE_0 = sqrt(sum(((Y_hat - Y)^2) *(1 - A))/sum(1 - A))
